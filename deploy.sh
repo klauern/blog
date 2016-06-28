88ae3d7a21a2b43b43658a686f75d404f3e8d54d
Nick Klauer <klauer@gmail.com>
2016-06-28 18:10:57 -0500

    fix deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public https://github.com/klauern/bio-page.git gh-pages
+git subtree push --prefix=public https://github.com/klauern/blog.git gh-pages
